05c29809ebbea7acc6aa0decc8cb0c18c5b23140
SamuelePassiatore <semmisno2@protonmail.com>
2023-02-23 15:12:33 +0100

    first commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="it">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>laravel comics | Home</title>
+</head>
+
+<body>
+    <h1>Home</h1>
+</body>
+
+</html>
---
+++ resources/views/tv.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="it">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>laravel comics | Tv</title>
+</head>
+
+<body>
+    <h1>Tv</h1>
+</body>
+
+</html>
