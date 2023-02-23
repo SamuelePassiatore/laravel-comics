9da8f2c70b3fbe43f754787a8aec4c4e4c8ef3c6
SamuelePassiatore <semmisno2@protonmail.com>
2023-02-23 15:21:39 +0100

    add static link at home blade

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -9,7 +9,21 @@
 </head>
 
 <body>
-    <h1>Home</h1>
+    <header>
+        <a href="{{ url('/') }}"><img src="" alt=""></a>
+        <ul>
+            <li><a href="{{ route('characters') }}">Characters</a></li>
+            <li><a href="{{ route('comics') }}">Comics</a></li>
+            <li><a href="{{ route('movies') }}">Movies</a></li>
+            <li><a href="{{ route('tv') }}">Tv</a></li>
+            <li><a href="{{ route('games') }}">Games</a></li>
+            <li><a href="{{ route('collectibles') }}">Collectibles</a></li>
+            <li><a href="{{ route('videos') }}">Videos</a></li>
+            <li><a href="{{ route('fans') }}">Fans</a></li>
+            <li><a href="{{ route('news') }}">News</a></li>
+            <li><a href="{{ route('shop') }}">Shop</a></li>
+        </ul>
+    </header>
 </body>
 
 </html>
